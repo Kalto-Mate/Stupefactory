15ba79019c50b72c5c7657c9d3846caafeee8d23
Kalto-Mate <Guillermo_Gonzalez_Sanchez@outlook.com>
2023-12-19 20:32:46 +0100

    Tilting Programatically

--- Assets/Scripts/PlayerInput.gd
+++ Assets/Scripts/PlayerInput.gd
@@ -1,15 +1,16 @@
 extends Node
-
+@export var PollTimer: Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	PollTimer.timeout.connect(PollInput)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_pressed("Tilt_positive"):
+
+func PollInput():
+	if Input.is_action_pressed("Tilt_positive"):
 		Signals.pi_tiltPositive.emit()
-	if Input.is_action_just_pressed("Tilt_negative"):
+	if Input.is_action_pressed("Tilt_negative"):
 		Signals.pi_tiltNegative.emit()
-	pass
