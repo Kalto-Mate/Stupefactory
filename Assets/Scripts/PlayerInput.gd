b8938464b9499661af0736a6d8d40fe0589e0c14
Kalto-Mate <Guillermo_Gonzalez_Sanchez@outlook.com>
2023-12-20 19:46:06 +0100

    New Rotation System
    Platform rotation is handled with a tween of dinamically calculated duration

--- Assets/Scripts/PlayerInput.gd
+++ Assets/Scripts/PlayerInput.gd
@@ -3,14 +3,15 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	PollTimer.timeout.connect(PollInput)
+	#PollTimer.timeout.connect(PollInput)
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	PollInput()
 
 func PollInput():
-	if Input.is_action_pressed("Tilt_positive"):
+	if Input.is_action_just_pressed("Tilt_positive"):
 		Signals.pi_tiltPositive.emit()
-	if Input.is_action_pressed("Tilt_negative"):
+	if Input.is_action_just_pressed("Tilt_negative"):
 		Signals.pi_tiltNegative.emit()
--- Assets/Scripts/SeeSaw.gd
+++ Assets/Scripts/SeeSaw.gd
@@ -1,29 +1,35 @@
-extends StaticBody2D
+extends AnimatableBody2D
 
-@export var maxTiltingDeg: float = 30
-var maxTiltingRad:float
-@export var tiltStepDeg: float = 15
-var tiltStepRad:float
+var SeeSaw_tween: Tween
 
-# Called when the node enters the scene tree for the first time.
+var PositionIndex: int = 0
+var MaxPositionIndex: int = 1
+var MinPositionIndex: int = -1
+
+@export var StepTime:float = .5
+var platformMaxAbsAngle: float = 30
+
 func _ready():
-	maxTiltingRad = deg_to_rad(maxTiltingDeg)
-	tiltStepRad = deg_to_rad(tiltStepDeg)
-	
 	Signals.pi_tiltPositive.connect(_tiltPositive)
 	Signals.pi_tiltNegative.connect(_tiltNegative)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _tilt(tiltAmmount:float):
-	var newAngle: float = self.rotation + tiltAmmount
-	var clampedNewAngle: float = clamp(newAngle,-maxTiltingRad,maxTiltingRad)
-	self.rotation = clampedNewAngle
-
 func _tiltPositive():
-	_tilt(tiltStepRad)
+	_tilt(1)
 func _tiltNegative():
-	_tilt(-1 * tiltStepRad)
+	_tilt(-1)
+
+func _tilt(direction: int):
+	var NewPositionIndex = PositionIndex + direction
+	NewPositionIndex = clamp(NewPositionIndex,MinPositionIndex,MaxPositionIndex)
+	PositionIndex = NewPositionIndex
+	_tweenToAngle(PositionIndex*platformMaxAbsAngle)
+
+func _tweenToAngle(TargetAngle:float):
+#Make sure to check if the tween already exists before creating a new one to avoid multiple instances
+#of the property being changed
+	if SeeSaw_tween:
+		SeeSaw_tween.kill()
+	SeeSaw_tween = get_tree().create_tween()
+	var AbsAngleDelta = abs(self.rotation_degrees - TargetAngle)
+	var adjustedTime = (AbsAngleDelta*StepTime) / platformMaxAbsAngle
+	SeeSaw_tween.tween_property(self,"rotation_degrees",TargetAngle,adjustedTime)
